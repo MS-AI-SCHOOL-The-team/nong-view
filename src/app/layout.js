8133af206594a17ca38efbf72637457d2a7fa4bc
IDKNWHORU <junho9341@gmail.com>
2024-09-30 18:19:20 +0900

    add ms clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import localFont from "next/font/local";
+import Script from 'next/script'
+
 import "./globals.css";
 
 const geistSans = localFont({
@@ -20,6 +22,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script id="microsoft-clarity-script" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_TAG_ID}");
+        `}
+      </Script>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
