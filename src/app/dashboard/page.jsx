6a025b213338a9b329f7f2e3a16372d1c97f5fcc
IDKNWHORU <junho9341@gmail.com>
2024-10-21 13:08:56 +0900

    add news section

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,9 +4,35 @@ import styles from "./page.module.css";
 import Prediction from "./Prediction";
 import ChatManual from "@/component/ChatManual";
 import { getChartData } from "@/actions/chart";
+import Link from "next/link";
 
 export default async function () {
   const data = await getChartData("배추");
+  const newsList = [{
+    link: "https://www.nongupin.co.kr/news/articleView.html?idxno=202104",
+    text: "한능평 ‘사육기간 단축부문’ 내년에도 열린다"
+  }, {
+    link: "https://www.nongupin.co.kr/news/articleView.html?idxno=202103",
+    text: "제발 20만원 약속이라도 지켜달라…넋 놓고 또 빚진다"
+  }, {
+    link: "https://www.nongupin.co.kr/news/articleView.html?idxno=202102",
+    text: "윤 대통령, 9월 집중호우 14개 읍면동 특별재난지역 선포"
+  }, {
+    link: "https://www.nongupin.co.kr/news/articleView.html?idxno=202100",
+    text: "한국형 2세대 스마트팜 조기 상용화 준비 ‘착착’"
+  }, {
+    link: "https://www.nongupin.co.kr/news/articleView.html?idxno=202099",
+    text: "데임·터짐 피해 ‘배’, 수확 후 선별·저장 주의해야"
+  }, {
+    link: "https://www.nongupin.co.kr/news/articleView.html?idxno=202098",
+    text: "저온성 필름으로 재배한 ‘천궁’"
+  }, {
+    link: "https://www.nongupin.co.kr/news/articleView.html?idxno=202096",
+    text: "젖뗀 돼지 설사 ‘박테리오파지’ 활용 기술로 줄여"
+  }, {
+    link: "https://www.nongupin.co.kr/news/articleView.html?idxno=202104",
+    text: "한능평 ‘사육기간 단축부문’ 내년에도 열린다"
+  }]
 
   return (
     <body className={styles.page}>
@@ -14,7 +40,11 @@ export default async function () {
         <Image src="/logo2.png" alt="농뷰 로고" width={150} height={63.75} quality={100} unoptimized />
         <section className={styles.headLine}>
           <h1 className={styles.headLineTitle}>오늘의 뉴스📢</h1>
-          <p className={styles.headLineContent}>정부, 온라인 판매 쌀, 상추, 사과 등 다소비 농산물 안전성 검사</p>
+          <article className={styles.headLineContent}>
+            <div className={styles.tickerWrapper}>
+              {newsList.map(({ link, text }, index) => <Link key={`news-${index}`} href={link} className={styles.tickerItem} target="_blank">{text}</Link>)}
+            </div>
+          </article>
         </section>
       </header>
       <hr className={styles.divider} />
